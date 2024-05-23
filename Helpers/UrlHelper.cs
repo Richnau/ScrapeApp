24fbc03ed59ffa98112df22d0b7190e36bfea494
Andreas Richnau <andreasrich@gmail.com>
2024-05-23 11:11:34 +0200

    Url validation

--- Helpers/UrlHelper.cs
+++ Helpers/UrlHelper.cs
@@ -1,17 +1,38 @@
-    class UrlHelper
-    {
-        public static string GetUrlFromUser()
-        {
-            Console.WriteLine("Please input the URL you wish to scrape:");
-            var inputUrl = Console.ReadLine() ?? string.Empty;
-            var sanitizedUrl = SanitizeUrl(inputUrl);
-            // TODO: implement validation
-            return sanitizedUrl;
-        }
+namespace ScrapeApp.Helpers;
+static class UrlHelper
+{
+	public static string GetUrlFromUser()
+	{
+		var validUrl = false;
+		var sanitizedUrl = string.Empty;
+		while (!validUrl)
+		{
+			Console.WriteLine("Please input the URL you wish to scrape:");
+			var inputUrl = Console.ReadLine() ?? string.Empty;
+			sanitizedUrl = SanitizeUrl(inputUrl);
+			validUrl = IsValidUrlAsync(sanitizedUrl).Result;
+		}
+		return sanitizedUrl;
+	}
 
-        private static string SanitizeUrl(string url)
-        {
-            // TODO: Implement sanitation. Drop querystring etc.
-            return url;
-        }
-    }
+	private static string SanitizeUrl(string url)
+	{
+		// TODO: Implement sanitation. Drop querystring etc.
+		return url;
+	}
+
+	public static async Task<bool> IsValidUrlAsync(string url)
+	{
+		using var httpClient = new HttpClient();
+		try
+		{
+			var response = await httpClient.GetAsync(url);
+			return response.IsSuccessStatusCode;
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine($"Error validating URL: {e.Message}");
+			return false;
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,23 +1,21 @@
-﻿namespace ScrapeApp
-{
-    class Program
-    {
-        static void Main()
-        {
-            bool keepAlive = true;
-            Console.WriteLine("Welcome to the Scrape App! (Press Ctrl-C to exit at any time.)");
-            while (keepAlive)
-            {
-                var url = UrlHelper.GetUrlFromUser();
-                // TODO: Use the url for scraping
+﻿namespace ScrapeApp;
+using ScrapeApp.Helpers;
 
-                Console.WriteLine("Would you like to scrape another URL? (Y/N)");
-                var input = Console.ReadLine();
-                keepAlive = input?.Equals("y", StringComparison.OrdinalIgnoreCase) == true
-                        || input?.Equals("yes", StringComparison.OrdinalIgnoreCase) == true;
-                
-                Console.WriteLine(url);
-            }
-        }
-    }
+class Program
+{
+	static void Main()
+	{
+		bool keepAlive = true;
+		Console.WriteLine("Welcome to the Scrape App! (Press Ctrl-C to exit at any time.)");
+		while (keepAlive)
+		{
+			var url = UrlHelper.GetUrlFromUser();
+			// TODO: Use the url for scraping
+
+			Console.WriteLine("Would you like to scrape another URL? (Y/N)");
+			var input = Console.ReadLine();
+			keepAlive = input?.Equals("y", StringComparison.OrdinalIgnoreCase) == true
+					|| input?.Equals("yes", StringComparison.OrdinalIgnoreCase) == true;
+		}
+	}
 }
